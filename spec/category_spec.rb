fff17cf267bb249ae54b13fb713e9e5a8049600e
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-02 20:54:34 +0100

    refactor specs

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe Category, type: :model do
   context 'creation' do
+    let(:category) { cat }
+
     describe 'valid params' do
-      let(:category) { build(:category) }
+      let(:cat) { build(:category) }
 
       it 'saves' do
         expect { category.save! }.to change { Category.count }.by(1)
@@ -11,7 +13,7 @@ describe Category, type: :model do
     end
 
     describe 'invalid params' do
-      let(:category) { build(:category, :invalid) }
+      let(:cat) { build(:category, :invalid) }
 
       it 'does not save' do
         expect { category.save! }.to raise_error(
