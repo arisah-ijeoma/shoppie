13788032f26baf1bb23f7ad8ecb7469a032f8703
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-02 21:08:44 +0100

    category edit spec

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe Category, type: :model do
+  let(:category) { cat }
+
+  context 'creation' do
+    describe 'valid params' do
+      let(:cat) { build(:category) }
+
+      it 'saves' do
+        expect { category.save! }.to change { Category.count }.by(1)
+      end
+    end
+
+    describe 'invalid params' do
+      let(:cat) { build(:category, :invalid) }
+
+      it 'does not save' do
+        expect { category.save! }.to raise_error(
+          ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank"
+        )
+      end
+    end
+  end
+
+  context 'editing' do
+    let(:cat) { create(:category, name: 'Originals') }
+
+    describe 'valid params' do
+      it 'updates correctly' do
+        category.name = 'Photocopy'
+        category.save!
+        expect(category.name).to eq('Photocopy')
+      end
+    end
+
+    describe 'invalid params' do
+      it 'updates correctly' do
+        category.name = ''
+        expect { category.save! }.to raise_error(
+          ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank"
+        )
+      end
+    end
+  end
+end
