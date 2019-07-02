4a19526d345c4a9de646fe3b7ac51c015209e2a5
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-02 20:52:30 +0100

    make spec pass

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :sub_categories, foreign_key: :parent_id
   has_many :children, through: :sub_categories, source: :child
+
+  validates :name, presence: true
 end
