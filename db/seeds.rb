da0a3c047e4dcfcac66bcdd8213875923c0605b5
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 17:56:24 +0100

    update seeds with admin user association

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-AdminUser.find_or_create_by!(email: 'admin@example.com') do |a|
+admin_user = AdminUser.find_or_create_by!(email: 'admin@example.com') do |a|
   a.password = 'password'
   a.password_confirmation = 'password'
 end
 
-category = Category.find_or_create_by!(name: 'Sample Category')
+category = Category.find_or_create_by!(name: 'Sample Category') do |c|
+  c.admin_user = admin_user
+end
 
-category_1 = Category.create(name: 'first_sub', category: category)
-category_2 = Category.create(name: 'second_sub', category: category, visible: false)
-category_3 = Category.create(name: 'third_sub', category: category)
+category_1 = Category.create(name: 'first_sub', category: category, admin_user: admin_user)
+category_2 = Category.create(name: 'second_sub', category: category, visible: false, admin_user: admin_user)
+category_3 = Category.create(name: 'third_sub', category: category, admin_user: admin_user)
 
 Product.create(name: 'product1', category: category)
 Product.create(name: 'product2', category: category_1)
