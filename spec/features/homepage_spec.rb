140cf08e6d60e86c22bfd5c527d4289e3f0dff3d
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 13:01:29 +0100

    update failing spec

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 describe 'home page', type: :feature do
-  let!(:category) { create(:category, name: 'Food') }
-  let!(:category) { create(:category, name: 'Games', visible: false) }
+  let!(:parent_category_1) { create(:category, name: 'Food') }
+  let!(:parent_category_2) { create(:category, name: 'Games', visible: false) }
+  let!(:child_category_11) { create(:category, name: 'Colors', category: parent_category_1 ) }
 
-  scenario 'visible and invisible categories are seen' do
+  scenario 'visible and invisible parent categories are seen' do
     visit root_path
     expect(page).to have_content('Food')
     expect(page).to have_content('Games')
+    expect(page).not_to have_content('Colors')
   end
 end
