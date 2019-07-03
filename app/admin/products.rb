6669e846071aa7196f888712ff0085f7190ed8f3
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 12:15:43 +0100

    remove certain filters

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register Product do
   permit_params :name, :description, :price, :status, :availability_date, :image, :category_id
 
+  remove_filter :updated_at, :created_at
+
   index do
     columns_to_exclude = %w[created_at updated_at image category_id]
     (Product.column_names - columns_to_exclude).each do |p|
