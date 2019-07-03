01b28812ba126a5d11a3c31d22b4283341d4a9d4
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 10:56:53 +0100

    make spec pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  STATUSES = %w[active inactive].freeze
+
+  validates :name, :price, presence: true
+  validates :status, inclusion: { in: STATUSES }
 end
