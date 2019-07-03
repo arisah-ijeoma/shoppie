e569fd0820e33a9d22f1632c0ed4d6b44eadc6cd
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 13:34:46 +0100

    add failing spec for products that show on homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,14 +3,27 @@
 require 'rails_helper'
 
 describe 'home page', type: :feature do
+  # Categories
   let!(:parent_category_1) { create(:category, name: 'Food') }
   let!(:parent_category_2) { create(:category, name: 'Games', visible: false) }
-  let!(:child_category_11) { create(:category, name: 'Colors', category: parent_category_1 ) }
+  let!(:child_category_1_1) { create(:category, name: 'Colors', category: parent_category_1 ) }
+
+  # Products
+  let!(:visible_parent_product) { create(:product, name: 'Afang', category: parent_category_1) }
+  let!(:hidden_parent_product) { create(:product, name: 'MK11', category: parent_category_2) }
+  let!(:visible_child_product) { create(:product, name: 'Green', category: child_category_1_1) }
 
   scenario 'visible and invisible parent categories are seen' do
     visit root_path
     expect(page).to have_content('Food')
     expect(page).to have_content('Games')
     expect(page).not_to have_content('Colors')
+  end
+
+  scenario 'products with categories marked visible are seen' do
+    visit root_path
+    expect(page).to have_content('Afang')
+    expect(page).to have_content('Green')
+    expect(page).not_to have_content('MK11')
   end
 end
