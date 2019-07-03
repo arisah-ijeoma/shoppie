c45526cc205f10888303f2e4f88d0d68d7ead385
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-04 00:10:58 +0100

    make spec pass

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,11 +5,10 @@ class CategoriesController < ApplicationController
   end
 
   def filter
-    @category = Category.find_by(id: params[:category])
+    @category = Category.find_by(category_id: params[:category_id]).category
     @products =
       if params[:category].blank?
-        category = Category.find_by(category_id: params[:category_id]).category
-        category.all_products
+        @category.all_products
       else
         Product.filter_by_sub_category(params[:category])
       end
