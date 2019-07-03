da740abaa358aad4b5cfc1e8d8efc6e03357542e
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 10:50:31 +0100

    add test for null constraint on product creation

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     status { %w[active inactive].sample }
     availability_date { Faker::Date.forward(23) }
     association(:category)
+
+    trait :invalid do
+      price { nil }
+    end
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,5 +9,13 @@ describe Product, type: :model do
         expect { product.save! }.to change { Product.count }.by(1)
       end
     end
+
+    describe 'invalid params' do
+      let(:product) { build(:product, :invalid) }
+
+      it 'does not save' do
+        expect { product.save!(validate: false) }.to raise_error(ActiveRecord::NotNullViolation)
+      end
+    end
   end
 end
