47e68ee9374744263177230325793c2299673e73
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-03 16:26:35 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@ end
 
 category = Category.find_or_create_by!(name: 'Sample Category')
 
-Category.create(name: 'first_sub', category: category)
-Category.create(name: 'second_sub', category: category)
-Category.create(name: 'third_sub', category: category)
+category_1 = Category.create(name: 'first_sub', category: category)
+category_2 = Category.create(name: 'second_sub', category: category, visible: false)
+category_3 = Category.create(name: 'third_sub', category: category)
+
+Product.create(name: 'product1', category: category)
+Product.create(name: 'product2', category: category_1)
+Product.create(name: 'product3', category: category_2)
+Product.create(name: 'product4', category: category_3)
