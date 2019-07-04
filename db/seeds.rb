4bc046992d43d8dd037153c707a569842fdf3c76
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-04 08:36:10 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,9 @@ end
 
 category_1 = Category.create(name: 'first_sub', category: category, admin_user: admin_user)
 category_2 = Category.create(name: 'second_sub', category: category, visible: false, admin_user: admin_user)
-puts 'categories', [category_1, category_2]
-category_3 = Category.new
-category_3.name = 'third_sub'
-category_3.category = category
-category_3.admin_user = admin_user
-  if category_3.save
-    puts 'created'
-  else
-    puts category_3.errors
-  end
+category_3 = Category.create(name: 'third_sub', category: category, admin_user: admin_user)
 
-Product.create(name: 'product1', category: category)
-Product.create(name: 'product2', category: category_1)
-Product.create(name: 'product3', category: category_2)
-Product.create(name: 'product4', category: category_3)
+Product.create(name: 'product1', category: category, price: '12.34')
+Product.create(name: 'product2', category: category_1, price: '25')
+Product.create(name: 'product3', category: category_2, price: '1.40')
+Product.create(name: 'product4', category: category_3, price: '17.54')
