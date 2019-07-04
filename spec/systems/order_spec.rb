657e130efbf91e3e937ac2aabb9a340c6fc18003
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-04 20:03:27 +0100

    modify spec expectations

--- spec/systems/order_spec.rb
+++ spec/systems/order_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'orders', type: :system, js: true do
   let!(:user) { create(:user) }
-  let!(:product) { create(:product, name: 'Cranberry', price: '14.90') }
+  let!(:product) { create(:product, name: 'Cranberry', price: '54.90') }
 
   scenario 'basket is updated when user adds to it' do
     visit '/users/sign_in'
@@ -13,10 +13,11 @@ describe 'orders', type: :system, js: true do
     # first item
     click_on 'Cranberry'
     click_on 'Add to Basket'
-    expect(page).to have_content('1')
+    expect(page).to have_selector('#order_count', visible: false, text: '1')
 
     # adding item twice increments count
     click_on 'Add to Basket'
-    expect(page).to have_content('2')
+    expect(page).not_to have_selector('#order_count', visible: false, text: '1')
+    expect(page).to have_selector('#order_count', visible: false, text: '2')
   end
 end
