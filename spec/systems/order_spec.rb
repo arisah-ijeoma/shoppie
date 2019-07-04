b56f84e4a3983eb286d07eaff89410af562ce574
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-04 17:07:35 +0100

    Add failing spec for number of items in basket

---
+++ spec/systems/order_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'orders', type: :system, js: true do
+  let!(:user) { create(:user) }
+  let!(:product) { create(:product, name: 'Cranberry', price: '14.90') }
+
+  scenario 'basket is updated when user adds to it' do
+    visit '/users/sign_in'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+
+    click_on 'Cranberry'
+    click_on 'Add to Basket'
+
+    within 'nav' do
+      expect(page).to have_content('1')
+    end
+  end
+end
