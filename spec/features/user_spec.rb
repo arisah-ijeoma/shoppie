40f356ad0047893303ee5ca4e5d0b7e7c67d3d67
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-04 12:17:07 +0100

    refactor code

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,12 +6,7 @@ describe 'user', type: :feature do
   let!(:user) { create(:user) }
 
   scenario 'logout button shows when user is signed in' do
-    visit '/users/sign_in'
-
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-
-    click_on 'Log in'
+    user_logs_in
     expect(page).to have_content('Log out')
   end
 
@@ -22,16 +17,19 @@ describe 'user', type: :feature do
   end
 
   scenario 'logout button does not show when user is not signed in' do
-    visit '/users/sign_in'
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_on 'Log in'
-
+    user_logs_in
     click_on 'Log out'
 
     visit root_path
 
     expect(page).to have_content('Log in')
     expect(page).not_to have_content('Log out')
+  end
+
+  def user_logs_in
+    visit '/users/sign_in'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
   end
 end
