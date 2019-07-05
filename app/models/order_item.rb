b532c4e9b0e03bd7c7f5073ab2702100fed41423
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-05 17:02:38 +0100

    style order page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,7 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  delegate :name, :price, :image, :admin_user, to: :product
+  delegate :name, :price, :image, :admin_user, :category, to: :product
 
   def sub_total
     price * quantity
