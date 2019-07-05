3df19b53b9303f368bc2ed16e7cf599c1e8799c2
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-05 12:26:10 +0100

    add spec for empty order page

---
+++ spec/features/order_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'orders', type: :feature do
+  let!(:user) { create(:user) }
+
+  scenario 'empty basket shows empty order page' do
+    login user
+    find('fa-shopping-basket').click
+
+    expect(page).to have_content('Your basket is empty')
+    expect(page).to have_link('Start shopping', href: root_path)
+  end
+end
