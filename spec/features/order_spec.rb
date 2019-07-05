c4932a1190a027bf064b1efc9ac83adb1753e07a
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-05 12:41:21 +0100

    fix failing spec setup

--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -4,10 +4,11 @@ require 'rails_helper'
 
 describe 'orders', type: :feature do
   let!(:user) { create(:user) }
+  let!(:product) { create(:product, name: 'Cranberry', price: '14.90') }
 
   scenario 'empty basket shows empty order page' do
     login user
-    find('.fa-shopping-basket').click
+    click_on 'Basket'
 
     expect(page).to have_content('Your basket is empty')
     expect(page).to have_link('Start shopping', href: root_path)
