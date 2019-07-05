b10d2e2359a06fa109bcd4f2cce348829a4d0f5a
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-05 15:50:07 +0100

    skeletal basket page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  delegate :name, :description, :price, :image, :admin_user, to: :product
+  delegate :name, :price, :image, :admin_user, to: :product
 end
