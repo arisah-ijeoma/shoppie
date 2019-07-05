870e9936bfef4ef5510080a1a29625a05859b5b9
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-05 08:58:02 +0100

    fix failing spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,16 @@ class ApplicationController < ActionController::Base
 
   before_action :get_order
 
+  def after_sign_in_path_for(resource)
+    route_params = Rails.application.routes.recognize_path(request.referrer)
+
+    if route_params[:action] == 'add_to_basket'
+      category_product_path(route_params[:category_id], route_params[:id])
+    else
+      super
+    end
+  end
+
   private
 
   def layout
