6a6bba16104bbbffe26f6fa2918a0373d144aedc
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-07-08 19:39:10 +0100

    tie mailer to app

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,11 +14,19 @@ class OrdersController < ApplicationController
 
   def update_user_details
     @user = current_user
-    name = params[:user][:name]
-    address = params[:user][:address]
+    user_name = params[:user][:name]
+    user_address = params[:user][:address]
+    admin_and_items = {}
 
-    if @user.update(name: name, address: address)
+    if @user.update(name: user_name, address: user_address)
       @order.order_items.where.not(status: 'Complete').update(status: 'Complete', deleted: true)
+
+      @order.order_items.each do |order_item|
+        admin_and_items[order_item.admin_user] = order_item
+      end
+
+      admin_and_items.each { |admin, _order_items| admin.order_notification }
+
       flash[:success] = 'Order successfully placed'
       redirect_to root_path
     else
